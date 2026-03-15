3996fa897b27dc64f31a52575b28af2ef1b4e10b
Madhu Verma <madhu110189@gmail.com>
2026-03-15 13:23:45 +0530

    Installed EF core bundles and setup db in program.cs and appsettings.json

---
+++ DBOperationsWithEFCore/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DBOperationsWithEFCore.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
+        { 
+
+        }
+    }
+}
--- DBOperationsWithEFCore/Program.cs
+++ DBOperationsWithEFCore/Program.cs
@@ -1,5 +1,11 @@
+using DBOperationsWithEFCore.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddDbContext<AppDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("AppDb"))
+);
 // Add services to the container.
 
 builder.Services.AddControllers();
