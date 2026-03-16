926b5145b436f588a15115ecbf26d18ee059cd54
Madhu Verma <madhu110189@gmail.com>
2026-03-16 21:53:32 +0530

    Added library controller to get all currencies and languages

---
+++ DBOperationsWithEFCore/Controllers/LibraryController.cs
@@ -0,0 +1,36 @@
+﻿using DBOperationsWithEFCore.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DBOperationsWithEFCore.Controllers
+{
+    [Route("api/library")]
+    [ApiController]
+    public class LibraryController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        public LibraryController(AppDbContext appDb) 
+        { 
+            this._context = appDb;
+        }
+
+        [HttpGet("currency")]
+        public async Task<IActionResult> GetAllCurrencies()
+        {
+            //var result = await _context.Currency.ToListAsync();
+            var result = await (from currencies in _context.Currency
+                                select currencies).ToListAsync();
+            return Ok(result);
+        }
+
+        [HttpGet("language")]
+        public async Task<IActionResult> GetAllLanguage()
+        {
+            //var result = await _context.Languages.ToListAsync();
+            var result = await (from languages in _context.Languages
+                                select  languages).ToListAsync();
+            return Ok(result);
+        }
+    }
+}
