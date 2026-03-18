aa9b4d006dd4280be12bd3f740b06bee96d4d625
Madhu Verma <madhu110189@gmail.com>
2026-03-18 10:39:49 +0530

    Added new controller to add new book through json body

---
+++ DBOperationsWithEFCore/Controllers/BooksController.cs
@@ -0,0 +1,26 @@
+﻿using DBOperationsWithEFCore.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DBOperationsWithEFCore.Controllers
+{
+    [Route("api/books")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly AppDbContext appDb;
+        public BooksController(AppDbContext appDbContext)
+        {
+            this.appDb = appDbContext;
+        }
+
+        [HttpPost("addbook")]
+        public async Task<IActionResult> InsertBooks([FromBody] Book bookmodel)
+        {
+            appDb.Books.Add(bookmodel);
+            await appDb.SaveChangesAsync();
+
+            return Ok(bookmodel);   
+        }
+    }
+}
