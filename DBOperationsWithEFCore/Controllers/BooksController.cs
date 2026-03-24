15762721b2e2b7e97cdca6fb2f0d365239624d42
Madhu Verma <madhu110189@gmail.com>
2026-03-24 10:54:19 +0530

    added func to hard delete single record from books table

--- DBOperationsWithEFCore/Controllers/BooksController.cs
+++ DBOperationsWithEFCore/Controllers/BooksController.cs
@@ -59,5 +59,27 @@ namespace DBOperationsWithEFCore.Controllers
             .SetProperty(p => p.Description, p => p.Description + " updated"));
             return Ok();
         }
+
+        [HttpDelete("deletebook/{id}")]
+        public async Task<IActionResult> deleteFromBook([FromRoute] int id)
+        {
+            #region delete method 1 - by 2 db hits
+            //var book = appDb.Books.FirstOrDefault(x=> x.Id ==id);
+            //if (book==null)
+            //{
+            //    return NotFound();
+            //}
+            //appDb.Books.Remove(book);
+            //await appDb.SaveChangesAsync();
+            #endregion
+
+            #region delete method 2 - by single db hit
+            var book = new Book { Id = id };
+            appDb.Entry(book).State = EntityState.Deleted;
+            await appDb.SaveChangesAsync();
+            #endregion
+
+            return Ok();
+        }
     }
 }
